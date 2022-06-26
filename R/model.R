7acfe6c1076c9b0c765f4cea97f1f45204f2cd09
Petros Paplomatas <84085141+PaplomatasP@users.noreply.github.com>
2022-06-26 10:14:04 +0200

    Delete R directory

--- R/model.R
+++
@@ -1,4 +0,0 @@
-#'@title A trained model
-#'@description An already trained ML model with the "lm" method using the default data from the package, to be used in the predict example{, and potentially by the user for further predictions}.
-
-"model"
